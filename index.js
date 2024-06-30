c592245a48e8b57c2127213e606c56fa685f00bb
abdul khaliq <abdallamakhtal@gmail.com>
2024-06-30 15:49:30 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Challenge 1: Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// Challenge 2: Random
+let random = Math.ceil(Math.random() * 10);
+
+// Challenge 3: Modulo
+let num3 = 24;  
+let num4 = 20;  
+let mod = num3 % num4;
+
+// Challenge 4: Max
+let max = Math.max(5, 10, 15, 20);  
